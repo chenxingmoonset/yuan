2e1a5bb380453b1a8022d1e2aead4d866afad06b
chenxing <67208373+chenxingmoonset@users.noreply.github.com>
2024-05-05 16:33:58 +0800

    更新 浏览器-deup.js

---
+++ deup/浏览器-deup.js
@@ -0,0 +1,52 @@
+/*Deup的rss订阅示例*/
+class webView extends Deup {
+  
+ config = {
+    name: 'deup浏览器',
+    logo: 'https://s2.loli.net/2023/08/17/dZeUNcuftSLaG41.png'
+  };
+  async check() {
+    return true;
+  }
+  
+  inputs={
+    url:{
+      label:"链接",
+      require:true,
+    },
+    logo:{
+      label:"图标",
+      require:true,
+    },
+  }
+    
+
+  async get(object) {
+        //$alert((await $storage.inputs).logo);
+        return {
+          id:object.id,
+          name:object.name,
+          remark:"浏览器测试",
+          options:{hideNavBar:false,},
+          url:(await $storage.inputs).url,
+          cover:object.cover,//(await $storage.inputs).logo,
+          thumbnail:(await $storage.inputs).logo,
+          poster:(await $storage.inputs).logo,
+        }
+    }
+
+  async list(object, offset, limit) {
+    return [{
+      id:"1",
+      type:"webview",
+      name:"浏览页",
+      cover:(await $storage.inputs).logo,
+      thumbnail:(await $storage.inputs).logo,
+      poster:(await $storage.inputs).logo,
+    }]
+  }
+
+  async search(object, keyword, offset, limit) {
+}
+}
+Deup.execute(new webView());
